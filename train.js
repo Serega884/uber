7408f866ada71bc21bbf6af9736b3a063514fef7
Serega884 <conwallaria@yandex.ru>
2021-08-19 22:38:27 +0300

    second

---
+++ train.js
@@ -0,0 +1,13 @@
+String.prototype.toAlternatingCase = function () {
+  
+  let arr=this.split('');
+ for(let i=0;i<arr.length;i++){
+       if(arr[i].toUpperCase!=arr[i]){
+           arr[i]=arr[i].toUpperCase();
+       }else
+       arr[i]=arr[i].toLowerCase();
+      
+   }
+
+}
+"string".toAlternatingCase();
